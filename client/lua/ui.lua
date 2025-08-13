bceb2ea2eb51fdf91439d4832e2cbebc0a43ad9c
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-13 17:41:19 +0200

    refactor: Update to use selene.ui.lml

--- client/lua/ui.lua
+++ client/lua/ui.lua
@@ -1,25 +1,12 @@
-local UI = require("selene.ui")
+local UI = require("selene.ui.lml")
 
-local scene = UI:LoadUI({
-    type = "Node",
-    name = "Illarion",
-    width = 1024,
-    height = 768,
-    children = {
-        { type = "Image", name = "BottomFrame", anchor = UI.ANCHOR_BOTTOM_LEFT, x = 0, y = -512, attributes = { src = "client/textures/illarion/ui/gui_bottom.png" } },
-        { type = "Image", name = "TopFrame", anchor = UI.ANCHOR_TOP_RIGHT, x = -256, y = 0, attributes = { src = "client/textures/illarion/ui/gui_top.png" } },
-        { type = "Image", name = "ChatBackground", anchor = UI.ANCHOR_BOTTOM_LEFT, x = 0, y = -370, attributes = { src = "client/textures/illarion/ui/gui_chat.png" } },
-    }
+local skin = UI.CreateSkin()
+skin:AddTexture("gui_bottom", "client/textures/illarion/ui/gui_bottom.png")
+skin:AddTexture("gui_top", "client/textures/illarion/ui/gui_top.png")
+skin:AddTexture("gui_chat", "client/textures/illarion/ui/gui_chat.png")
+local hud = UI.LoadUI("client/ui/illarion/hud.xml", {
+    skin = skin
 })
-UI.Root:AddChild(UI:CreateUI(scene))
-
---local topFrame = mainContainer.image("client/textures/illarion/ui/gui_top.png")
---ui.addToTree(mainContainer)
---local counter = mainContainer.image("client/textures/illarion/ui/gui_counter.png")
---local statusBackground = mainContainer.image("client/textures/illarion/ui/gui_status.png")
---local statusHealth = statusBackground.image("client/textures/illarion/ui/status_health.png")
---local statusFood = statusBackground.image("client/textures/illarion/ui/status_food.png")
---local statusMana = statusBackground.image("client/textures/illarion/ui/status_mana.png")
---local rightShowcase = mainContainer.image("client/textures/illarion/ui/showcase_back.png")
---local leftShowcase = mainContainer.image("client/textures/illarion/ui/showcase_back.png")
---local speakNormal = mainContainer.image("client/textures/illarion/ui/speak_normal.png")
+for i, widget in ipairs(hud) do
+    UI.Root:AddChild(widget)
+end
