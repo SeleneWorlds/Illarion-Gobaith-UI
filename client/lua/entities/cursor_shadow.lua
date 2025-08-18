d200b8e627b6ca51e4bb385b1da51005db9b6b1e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-18 17:49:55 +0200

    fix: Use new Alpha property instead of SetColor

--- client/lua/entities/cursor_shadow.lua
+++ client/lua/entities/cursor_shadow.lua
@@ -4,8 +4,7 @@ function m.TickEntity(Entity, Data, Delta)
     Data.TimePassed = (Data.TimePassed or 0) + Delta
     local visual = Entity:GetComponent("illarion:visual")
     if visual then
-        local alpha = 1 - (Data.TimePassed / 0.1)
-        visual:SetColor(1, 1, 1, alpha)
+        visual.Alpha = 1 - (Data.TimePassed / 0.1)
     end
 end
 
