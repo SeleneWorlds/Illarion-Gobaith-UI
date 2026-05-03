80ec7f413b129caeb9ee5dd547fed16d36388f3b
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-05-03 15:40:29 +0200

    feat: Add window scaling and aspect ratio

--- client/lua/viewport.lua
+++ client/lua/viewport.lua
@@ -1,2 +1,6 @@
 local Camera = require("selene.camera")
+local Game = require("selene.game")
 Camera.SetViewport(0, 0, 839, 419)
+
+Game.SetWindowAspectRatio(4, 3)
+Game.SetWindowScaling("UNIFORM", 1024, 768)
